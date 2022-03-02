03811d1b0763e2ff723a7cde84e9d26b32137d94
aspandhillon <97080228+aspandhillon@users.noreply.github.com>
2022-03-02 12:11:32 +0530

    Add files via upload

---
+++ assignment4 output.py
@@ -0,0 +1,79 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\maila\Desktop\assignment3.py ================
+ANSWER 1
+Move disk 1 from rod A to rod C
+Move disk 2 from rod A to rod B
+Move disk 1 from rod C to rod B
+Move disk 3 from rod A to rod C
+Move disk 1 from rod B to rod A
+Move disk 2 from rod B to rod C
+Move disk 1 from rod A to rod C
+
+
+ANSWER 2
+Enter the size of pascals triangle 10
+USING FOR LOOP
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+
+
+
+USING WHILE LOOP
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+
+
+
+USING RECURSSIONS
+         1 
+        1 1 
+       1 2 1 
+      1 3 3 1 
+     1 4 6 4 1 
+    1 5 10 10 5 1 
+   1 6 15 20 15 6 1 
+  1 7 21 35 35 21 7 1 
+ 1 8 28 56 70 56 28 8 1 
+1 9 36 84 126 126 84 36 9 1 
+
+
+ANSWER 3
+Enter 2 numbers: 10 20
+a. Checking if the quotient and remainder is a callable value or not.
+False
+False
+<b> The quotient is zero
+<c> Filtered out numbers that are greater than 4 are : [14, 15, 5, 15, 6, 16]
+<d> Set: {5, 6, 14, 15, 16}
+<e> Immutable set: frozenset({16, 5, 6, 14, 15})
+<f> Hash value of the max value from the above set: 16
+
+
+ANSWER 4 
+Object created
+The name of the student it Priyanka and SID is 21103051.
+Object destroyed
+
+
+ANSWER 5
+<a> The updated salary of Mehak is 70000 
+<b>Record of Viren deleted
+
